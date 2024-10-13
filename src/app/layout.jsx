2fd0a11d5cf631a827eb398ef7a3e347a7fbf449
Anjesh <bhattaraianjesh123@gmail.com>
2024-10-13 15:57:16 +1030

    "npx create-next-app@14.2.4 . " : Next.js app with dependencies and boilerplate

---
+++ src/app/layout.jsx
@@ -0,0 +1,51 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "animate.css";
+import "aos";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+const inter = Inter({ subsets: ["latin"] });
+import { ChakraProvider } from "@chakra-ui/react";
+
+export const metadata = {
+  title: {
+    default: "UPC",
+    template: "%s | Next.js 14",
+  },
+  description: "UPC",
+};
+
+export default function RootLayout({ children }) {
+
+ 
+  return (
+    <html lang="en">
+   
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://unpkg.com/aos@2.3.1/dist/aos.css"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+        
+      </head>
+      <body className="RootLayout" style={{ backgroundColor: "#000", }}>
+        <ChakraProvider>{children}</ChakraProvider>
+      </body>
+    </html>
+  );
+}
+
---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+   <>
+   <Text>
+    Test
+   </Text>
+   </>
+  );
+}
